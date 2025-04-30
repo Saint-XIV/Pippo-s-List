3ffe1e4c994810a58233dddf3c5215272640581e
Pippo <68723215+Saint-XIV@users.noreply.github.com>
2025-04-29 17:57:10 -0700

    Add files via upload

---
+++ pippo's list.lua
@@ -0,0 +1,276 @@
+-- Pippo's List
+
+local function makeIterator()
+    local index
+
+    return function ( t, _, lastValueReturned )
+        index = ( lastValueReturned and index + 1 ) or 1
+        if index <= #t then return t[ index ] end
+    end
+end
+
+
+--- @class Pip.List
+--- @field private __index table
+--- @field private __call function
+--- @field private __add function
+--- @field private __tostring function
+local list = {}
+list.__call = makeIterator()
+list.__index = list
+
+
+-- Adding Data
+
+--- @param value any
+function list:pushFront( value )
+    table.insert( self, value, 1 )
+end
+
+
+--- @param value any
+function list:pushBack( value )
+    table.insert( self, value )
+end
+
+
+--- @param value any
+function list:append( value )
+    table.insert( self, value )
+end
+
+
+function list:__add( right )
+    if type( right ) == "table" and getmetatable( right ) == list then
+        for _, item in ipairs( right ) do
+            table.insert( self, item )
+        end
+    else
+        table.insert( self, right )
+    end
+end
+
+
+-- Deleting Data
+
+--- @param index integer
+function list:swapbackPop( index )
+    local backItem = self[ #self ]
+    self[ index ] = backItem
+    table.remove( self, #self )
+end
+
+
+--- @param index integer
+function list:popAt( index )
+    table.remove( self, index )
+end
+
+
+--- @return any
+function list:popFront()
+    return table.remove( self, 1 )
+end
+
+
+--- @return any
+function list:popBack()
+    return table.remove( self, #self )
+end
+
+
+--- @param value any
+function list:erase( value )
+    for index, item in ipairs( self ) do
+        if item == value then
+            table.remove( self, index )
+        end
+    end
+end
+
+
+--- @param value any
+function list:swapbackErase( value )
+    for index, item in ipairs( self ) do
+        if item == value then
+            local backItem = self[ #self ]
+
+            self[ index ] = backItem
+            table.remove( self, #self )
+
+            return
+        end
+    end
+end
+
+
+function list:clear()
+    for index = #self, 1, -1 do
+        table.remove( self, index )
+    end
+end
+
+
+--- @return any
+function list:popRandom()
+    local value = self:getRandom()
+    self:erase( value )
+    return value
+end
+
+
+-- Reading Data
+
+--- @return any
+function list:getFront()
+    return self[ 1 ]
+end
+
+
+--- @return any
+function list:getBack()
+    return self[ #self ]
+end
+
+
+--- @return boolean
+function list:isEmpty()
+    return #self == 0
+end
+
+
+--- @param value any
+--- @return integer?
+function list:getIndex( value )
+    for index, item in ipairs( self ) do
+        if value == item then return index end
+    end
+
+    return nil
+end
+
+
+--- @param value any
+--- @return boolean
+function list:has( value )
+    for _, item in ipairs( self ) do
+        if item == value then return true end
+    end
+
+    return false
+end
+
+function list:getRandom()
+    return self[ math.random( #self ) ]
+end
+
+
+function list:__tostring()
+    local result = "["
+    for _, item in ipairs( self ) do
+        result = result..tostring( item )..", "
+    end
+
+    result = result:sub( 1, #result - 2 ).."]"
+
+    return result
+end
+
+-- Testing Data
+
+--- @return any
+function list:min()
+    local min
+
+    for _, item in ipairs( self ) do
+        if not min or item < min then min = item end
+    end
+
+    return min
+end
+
+
+--- @return any
+function list:max()
+    local max
+
+    for _, item in ipairs( self ) do
+        if not max or item > max then max = item end
+    end
+
+    return max
+end
+
+
+--- @param func fun( value : any ) : boolean
+--- @return boolean
+function list:any( func )
+    for _, item in ipairs( self ) do
+        if func( item ) then return true end
+    end
+
+    return false
+end
+
+
+--- @param func fun( value : any ) : boolean
+--- @return boolean
+function list:all( func )
+    for _, item in ipairs( self ) do
+        if not( func( item ) ) then return false end
+    end
+
+    return true
+end
+
+
+-- Constructors
+
+--- @param func fun( value : any ) : any
+--- @return Pip.List
+function list:map( func )
+    local mappedList = makeList()
+
+    for _, item in ipairs( self ) do
+        local result = func( item )
+        if result then mappedList:append( result ) end
+    end
+
+    return mappedList
+end
+
+
+--- @param func fun( value : any ) : boolean
+--- @return Pip.List
+function list:filter( func )
+    local filteredList = makeList()
+
+    for _, item in ipairs( self ) do
+        if func( item ) then filteredList:append( item ) end
+    end
+
+    return filteredList
+end
+
+
+--- @return Pip.List
+function list:duplicate()
+    return makeList( unpack( self ) )
+end
+
+
+--- @return Pip.List
+function _G.makeList( ... )
+    local newList = { ... }
+    return setmetatable( newList, list )
+end
+
+
+-- Wildcard!
+
+function list:shuffle()
+    for index = #self, 2, -1 do
+        local random = math.random( index )
+        self[ index ], self[ random ] = self[ random ], self[ index ]
+    end
+end
